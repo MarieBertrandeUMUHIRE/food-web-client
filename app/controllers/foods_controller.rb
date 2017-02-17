de5113554dbe050442b0bd28702712a8c276186f
MarieBertrandeUMUHIRE <mariebertrande.umuhire@gmail.com>
2017-02-17 11:08:10 +0200

    I have added PORO

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,7 +4,9 @@ class FoodsController < ApplicationController
   end
 
   def show
-    @food = Unirest.get("http://localhost:3000/api/v2/foods/#{params[:id]}.json").body
+    food_hash = Unirest.get("http://localhost:3000/api/v2/foods/#{params[:id]}.json").body
+    @food = Food.new(food_hash)
+
   end
 
   def new
@@ -13,5 +15,18 @@ class FoodsController < ApplicationController
   def create
    @food = Unirest.post("http://localhost:3000/api/v2/foods.json", :headers => {"Accept"=> "application/json"}, :parameters => {:ingredient => params[:ingredient], :spice => params[:spice], :measurement=> params[:measurement],}).body
    redirect_to "/foods/#{@food['id']}"
+ end
+ def edit
+   @food = Unirest.get("http://localhost:3000/api/v2/foods/#{params[:id]}.json").body
+ end
+ 
+ def update
+   @food = Unirest.patch("http://localhost:3000/api/v2/foods/#{params[:id]}.json", :headers => {"Accept"=> "application/json"}, :parameters => {:ingredient => params[:ingredient], :spice => params[:spice], :measurement=> params[:measurement],}).body
+   redirect_to "/foods/#{@food['id']}"
+ end
+ 
+ def destroy
+  @food = Unirest.delete("http://localhost:3000/api/v2/foods/#{params[:id]}.json").body
+   redirect_to "/foods"
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
  get "/foods", to: 'foods#index'
- post "/foods", to: 'foods#index'
+ get "/foods", to: 'foods#new'
+ get "/foods/:id", to: 'foods#show'
  post "/foods", to: 'foods#create'
  patch "/foods/:id", to: 'foods#update'
- get "/foods/:id", to: 'foods#new'
- post "/foods/:id", to: 'foods#create'
+ get "/foods/:id/edit", to: 'foods#edit'
+ delete "/foods/:id", to: 'foods#destroy'
+
 end
